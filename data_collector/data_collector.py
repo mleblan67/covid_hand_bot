9972235bca41d7f7b5ac3b25cc555865bb4a7cc8
mleblan67 <mleblan67@charlottesvilleschools.org>
2020-04-28 15:43:24 -0400

    Figure out what side is up

--- data_collector/data_collector.py
+++ data_collector/data_collector.py
@@ -1,29 +1,44 @@
 from time import sleep
 import Adafruit_LSM303
+import numpy as np
 
 # Create a LSM303 instance.
 lsm303 = Adafruit_LSM303.LSM303()
 
 #find magnitude of acceleration vector
-def magnitude(current,previous):
+def magnitude(vector):
     distance = 0
-    for i in range(len(current)):
-        distance += (current[i] - previous[i])**2
+    for i in range(len(vector)):
+        distance += (vector[i])**2
     return distance**0.5
 
+def single_distance(current,previous):
+    distance = [0,0,0]
+    for i in range(len(current)):
+        distance[i] = current[i] - previous[i]
+    return distance
+
+axis = ["X","Y","Z"]
 previous_accel = [0,0,0]
 #tell if acceleration magnitude is a real movement
-movement_tolerance = 17
+movement_tolerance = 180
 while True:
     # Read the X, Y, Z axis acceleration values and print them.
     accel, mag = lsm303.read()
-    # Grab the X, Y, Z components from accel and mag
-    accel_x, accel_y, accel_z = accel
-    mag_x, mag_y, mag_z = mag
-    movement = magnitude(accel,previous_accel)
+    accel,mag = list(accel),list(mag)
+    #absolute value to find biggest value
+    mag = np.absolute(mag)
+    movement = single_distance(accel,previous_accel)
+    total_magnitude = magnitude(movement)
     #get rid of noise and unimportant movements
-    if movement > movement_tolerance:
-        print("MOVED!")
+    if total_magnitude > movement_tolerance:
+        #print("MOVED! \t X:%s Y:%s Z:%s \t Xmag:%s Ymag:%s Zmag:%s"%(accel_x,accel_y,accel_z,mag_x,mag_y,mag_z))
+        #find which side is facing up based on biggest value
+        side_up = np.where(mag == np.amax(mag))[0][0]
+        direction = np.where(movement == np.amax(movement))[0][0]
+        #print(axis[side_up])
+        #print(axis[direction])
 
+        
     previous_accel = accel
-    sleep(0.1)
+    sleep(0.2)
